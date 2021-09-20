15ef6db2fe6595d3c9b78a809253bc9b789a32fc
Jesse <jharlan628@gmail.com>
2021-09-20 11:41:11 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+var num1 = 31;
+var num2 = 2;
+var num3 = 12;
+var num4 = 8;
 
-// Write your code here
+var multiply = num1 * num2
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+var mod = num3 % num4;
+
+var max = Math.max(1, 2, 5, 8, 10, 12, 15, 20);
